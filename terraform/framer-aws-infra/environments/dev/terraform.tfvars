c7cefb3cab6c953df4e1723709cb033d3cd9fb2d
Bhuvan <bhuvanbokka20@cse.iiitp.ac.in>
2026-07-29 10:28:28 +0530

    framer-pf: Restructure terraform project

---
+++ terraform/framer-aws-infra/environments/dev/terraform.tfvars
@@ -0,0 +1,3 @@
+aws_region = "eu-north-1"
+vpc_cidr = "10.2.0.0/16"
+instance_type = "t3.micro"
--- terraform/framer-aws-infra/main.tf
+++ terraform/framer-aws-infra/main.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0"
-    }
-  }
-}
-
-# Configure the AWS Provider
-provider "aws" {
-  region = "eu-north-1"
-}
-
 # Create a VPC
 resource "aws_vpc" "framer-pf-vpc" {
   cidr_block = "10.2.0.0/16"
